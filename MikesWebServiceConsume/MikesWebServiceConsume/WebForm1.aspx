052955f87e7da9bbc87f8f442f7fd35d160d76a3
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-08-08 19:05:10 +0100

    Add files via upload

---
+++ MikesWebServiceConsume/MikesWebServiceConsume/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MikesWebServiceConsume.WebForm1" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td><asp:TextBox runat="server" ID="txtID"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><asp:TextBox runat="server" ID="txtName"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>City</td>
+            <td><asp:TextBox runat="server" ID="txtCity"></asp:TextBox></td>
+        </tr>
+
+    </table>
+        <asp:Label runat="server" id="lblMessage">
+        </asp:Label>
+        <asp:Button runat="server" ID="btnInsert" OnClick="btnInsert_Click" Text="Insert" />
+        <asp:Button runat="server" ID="btnUpdate" OnClick="btnUpdate_Click" Text="Update" />
+        <asp:Button runat="server" ID="btnDelete" OnClick="btnDelete_Click" Text="Delete" />
+        <asp:Button runat="server" ID="btnSelect" OnClick="btnSelect_Click" Text="Select" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ MikesWebServiceConsume/MikesWebServiceConsume/WebForm1.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace MikesWebServiceConsume
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            MikesServiceRef.TestService objTest = new MikesServiceRef.TestService();
+            int ret = objTest.InsertDetail(txtName.Text, txtCity.Text);
+            if (ret > 0)
+            {
+                lblMessage.Text = "Record Inserted Successfully";
+                txtID.Text = "";
+                txtName.Text = "";
+                txtCity.Text = "";
+            }
+            else
+            {
+                lblMessage.Text = "Error while inserting record";
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MikesServiceRef.TestService objTest = new MikesServiceRef.TestService();
+            int ret = objTest.UpdateDetail(Convert.ToInt32(txtID.Text), txtName.Text, txtCity.Text);
+            if (ret > 0)
+            {
+                lblMessage.Text = "Record update Successfully";
+                txtID.Text = "";
+                txtName.Text = "";
+                txtCity.Text = "";
+            }
+            else
+            {
+                lblMessage.Text = "Error while updating record";
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            MikesServiceRef.TestService objTest = new MikesServiceRef.TestService();
+            int ret = objTest.DeleteRecord(Convert.ToInt32(txtID.Text));
+            if (ret > 0)
+            {
+                lblMessage.Text = "Record delete Successfully";
+                txtID.Text = "";
+                txtName.Text = "";
+                txtCity.Text = "";
+            }
+            else
+            {
+                lblMessage.Text = "Error while deleting record";
+            }
+        }
+
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            MikesServiceRef.TestService objTest = new MikesServiceRef.TestService();
+            DataSet ret = objTest.GetDetialByID(Convert.ToInt32(txtID.Text));
+            if (ret.Tables.Count > 0)
+            {
+                if (ret.Tables[0].Rows.Count > 0)
+                {
+                    //add the rows to the tables using the column names below.
+
+                    txtName.Text = ret.Tables[0].Rows[0]["PersonName"].ToString();
+                    txtCity.Text = ret.Tables[0].Rows[0]["PersonCity"].ToString();
+                }
+                else
+                {
+                    lblMessage.Text = "Record not found";
+                }
+            }
+        }
+    }
+}
